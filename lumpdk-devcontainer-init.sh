b9f9bad85db6f1f34b70a49cd549d58ffcb7a54d
lgisler <48827180+lgisler@users.noreply.github.com>
2025-07-18 14:17:16 +0200

    Add LumPDK devcontainer init script

---
+++ lumpdk-devcontainer-init.sh
@@ -0,0 +1,28 @@
+#/usr/bin/env bash
+
+set -euxo pipefail
+
+sudo chown -R "${USER}":"${USER}" "${HOME}"
+sudo chown -R "${USER}":"${USER}" /nix
+sudo apt-get update
+sudo apt-get install -y firefox zsh vim
+
+LUM="${HOME}/LumPDK"
+"${LUM}/bootstrap.py"
+"${LUM}/scripts/install/install_helix.sh"
+source "${HOME}/.nix-profile/etc/profile.d/nix.sh"
+ln -s "${HOME}/.config" "${LUM}/.config"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+cat <<EOF > "${HOME}/.zshrc"
+export ZSH="\${HOME}/.oh-my-zsh"
+ZSH_THEME="evan"
+HOST="dkr"
+plugins=(git bazel)
+source "\${ZSH}/oh-my-zsh.sh"
+alias cdl="cd \${HOME}/LumPDK/"
+alias bg="bzr gazelle"
+alias bbq="bzt --config=quality"
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/\$(id -u)/bus
+EOF
